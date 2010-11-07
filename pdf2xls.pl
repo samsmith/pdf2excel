0f089c058a0d56da3da6a089f0b30778ce8024d6
Sam Smith <github.com@msmith.net>
2010-11-07 23:28:57 +0000

    contact

--- pdf2xls.pl
+++ pdf2xls.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# license, BSD
+# license, BSD.  written by s@msmith.net
 
 # needs convert from imagemagick in the path
 
@@ -335,7 +335,7 @@ For programmers:
         	<li>use Spreadsheet::WriteExcel;</li></ul></li>
 
 </ul>
-<p>If you like this, you may want to sign up for notifications from the <a href="http://www.linkedgov.org/">LinkedGov project</a> run by friends .</p>
+<p>If you like this, you may want to sign up for notifications from the <a href="http://www.linkedgov.org/">LinkedGov project</a> run by friends .</p>. 
 <p />
 <a href="http://www.mysociety.org/"><img border="0" src="http://services.disruptiveproactivity.com/images/hostedbymysociety.png"></a>
 EOhtml
